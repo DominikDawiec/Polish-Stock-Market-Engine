3f0c6d01c622b6b8385a7062be2749a4117016cb
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-10-17 20:15:35 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+import streamlit as st
+     
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.offline import init_notebook_mode, iplot
+import plotly.graph_objs as go
+import plotly.io as pio
+pio.templates.default = "plotly"
+
+from datetime import date
+
+import matplotlib.pyplot as plt
+import warnings
+
+import xlsxwriter
+import io
+
+# import warnings
+warnings.filterwarnings('ignore')
+
+import yfinance as yf
