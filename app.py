b1f5899d53016eb85349511c315f1641ebef3655
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-10-17 21:41:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,11 +65,50 @@ if option:
      st.plotly_chart(fig)
      
      
-     fig = go.Figure(data=[go.Candlestick(x=hist.index,
-                open=hist['Open'],
-                high=hist['High'],
-                low=hist['Low'],
-                close=hist['Close'])])
-     st.plotly_chart(fig)
-
      
+     #EMA
+     df['EMA_9'] = df['Close'].ewm(9).mean().shift()
+     df['EMA_22'] = df['Close'].ewm(22).mean().shift()
+     #SMA
+     df['SMA_5'] = df['Close'].rolling(5).mean().shift()
+     df['SMA_10'] = df['Close'].rolling(10).mean().shift()
+     df['SMA_15'] = df['Close'].rolling(15).mean().shift()
+     df['SMA_30'] = df['Close'].rolling(30).mean().shift()
+     #RSI14
+     def RSI(df, n=14):
+         close = df['Close']
+         delta = close.diff()
+         delta = delta[1:]
+         pricesUp = delta.copy()
+         pricesDown = delta.copy()
+         pricesUp[pricesUp < 0] = 0
+         pricesDown[pricesDown > 0] = 0
+         rollUp = pricesUp.rolling(n).mean()
+         rollDown = pricesDown.abs().rolling(n).mean()
+         rs = rollUp / rollDown
+         rsi = 100.0 - (100.0 / (1.0 + rs))
+         return rsi
+
+     df['RSI'] = RSI(df).fillna(0)
+
+     EMA_12 = pd.Series(df['Close'].ewm(span=12, min_periods=12).mean())
+     EMA_26 = pd.Series(df['Close'].ewm(span=26, min_periods=26).mean())
+     df['MACD'] = pd.Series(EMA_12 - EMA_26)
+     df['MACD_signal'] = pd.Series(df.MACD.ewm(span=9, min_periods=9).mean())
+     
+#####################################
+
+     fig = go.Figure()
+     fig.add_trace(go.Scatter(x=df.index, y=df.EMA_9, name='EMA 9'))
+     fig.add_trace(go.Scatter(x=df.index, y=df.EMA_22, name='EMA 22'))
+     fig.add_trace(go.Scatter(x=df.index, y=df.SMA_5, name='SMA 5'))
+     fig.add_trace(go.Scatter(x=df.index, y=df.SMA_10, name='SMA 10'))
+     fig.add_trace(go.Scatter(x=df.index, y=df.SMA_15, name='SMA 15'))
+     fig.add_trace(go.Scatter(x=df.index, y=df.SMA_30, name='SMA 30'))
+     fig.add_trace(go.Scatter(x=df.index, y=df.Close, name='Close', opacity=0.3))
+     st.plotly_chart(fig)
+     
+##############################################
+
+     fig = go.Figure(go.Scatter(x=df.index, y=df.RSI, name='RSI'))
+     st.plotly_chart(fig)
