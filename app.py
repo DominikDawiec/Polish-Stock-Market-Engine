fab7e6d891a0338c82b78c2014af08ec1933085b
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-10-18 10:29:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,15 +25,7 @@ import yfinance as yf
 
 ###############################################################
 
-col1, col2 = st.columns([3, 1])
 
-with col1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
      
      
 ###############################################################
@@ -68,8 +60,21 @@ if option:
              ])
          )
      )
+     
+     
+     ######################################################################
+     
+     col1, col2 = st.columns([3, 1])
 
-     st.plotly_chart(fig)
+     with col1:
+        st.header("Name 1")
+        st.plotly_chart(fig)
+
+     with col2:
+        st.header("Name 2")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+
+     
      
      ######################################################################
      
