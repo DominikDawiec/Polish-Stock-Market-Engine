0be6bd9b178a603bf849fee70626a8c3817ef5f8
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2022-10-19 18:47:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ with st.sidebar:
 
 
 def plot():
-     fig = go.Figure([go.Scatter(x=hist.index, y=(hist['Open'], hist['Close'])              )])
+     fig = go.Figure([go.Scatter(x=hist.index, y=(hist['Open', 'Close'])              )])
 
      fig.update_xaxes(rangeslider_visible=True, rangeselector=dict(
           buttons=list([
