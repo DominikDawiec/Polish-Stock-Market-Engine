cdcf896106af14bffd80806010cf0a80004c9f44
Dominik Dawiec <105276892+DominikDawiec@users.noreply.github.com>
2023-04-10 15:42:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(layout="centered", page_icon="📈", page_title="Polish Stock
 # display title
 st.title("📈 Polish Stock App")
 
-companies = ['11B.WA', 'ABE.WA', 'ACG.WA', 'ACT.WA', 'AGO.WA', 'ALL.WA', 'ALR.WA', 'ALE.WA', 'AML.WA', 'AMB.WA', 'AMC.WA', 'EAT.WA', 'APT.WA', 'ATC.WA', 'ASB.WA', 'ASB.WA', 'ACP.WA', 'ASE.WA', 'ATT.WA', '1AT.WA', 'APR.WA', 'BFT.WA', 'BMX.WA', 'BML.WA', 'BIO.WA', 'BNP.WA', 'LWB.WA', 'BRS.WA', 'BOS.WA', 'BOW.WA', 'BDX.WA', 'BMC.WA', 'TOR.WA', 'CCC.WA', 'CDR.WA', 'CIE.WA', 'CIG.WA', 'CLN.WA', 'COG.WA', 'CMR.WA', 'CMP.WA', 'CRJ.WA', 'CPS.WA', 'DAT.WA', 'DCR.WA', 'DVL.WA', 'DNP.WA', 'DOM.WA', 'ECH.WA', 'ENA.WA', 'ENT.WA', 'ERB.WA', 'EUR.WA', 'FRO.WA', 'FTE.WA', 'GPW.WA', 'GRN.WA', 'GEA.WA', 'GRN.WA', 'ATT.WA', 'GPP.WA', 'BHW.WA', 'HUG.WA', 'ING.WA', 'INK.WA', 'CAR.WA', 'JSW.WA', 'KER.WA', 'KTY.WA', 'KGH.WA', 'KGN.WA', 'KRU.WA', 'LVC.WA', 'LPP.WA', 'LWB.WA', 'MAB.WA', 'MBK.WA', 'MCI.WA', 'MDG.WA', 'MRC.WA', 'MIL.WA', 'MRB.WA', 'MLS.WA', 'MBR.WA', 'MOC.WA', 'MSZ.WA', 'NEU.WA', 'NWG.WA', 'OND.WA', 'OPN.WA', 'OPL.WA', 'PCR.WA', 'PCF.WA', 'PBX.WA', 'PEO.WA', 'PEP.WA', 'PCO.WA', 'PGE.WA', 'PEN.WA', 'PKN.WA', 'PKO.WA', 'PKP.WA', 'PLW.WA', 'PXM.WA', 'PZU.WA', 'R22.WA', 'RBW.WA', 'RWL.WA', 'RVU.WA', 'SNK.WA', 'SPL.WA', 'SCP.WA', 'SEL.WA', 'SLV.WA', 'SHO.WA', 'SNK.WA', 'SPR.WA', 'STX.WA', 'STP.WA', 'STS.WA', 'SNX.WA', 'SGN.WA', 'SNK.WA', 'TPE.WA', 'TIM.WA', 'TOR.WA', 'TOA.WA', 'TSG.WA', 'UNT.WA', 'VRC.WA', 'VGO.WA', 'VOT.WA', 'VOX.WA', 'VRG.WA', 'WWL.WA', 'WLT.WA', 'WPL.WA', 'WTN.WA', 'XTB.WA', 'CCC.WA', 'ETFBM40TR.WA', 'ETFBNDXPL.WA', 'ETFBS80TR.WA', 'ETFBSPXPL.WA', 'ETFBTBSP.WA', 'ETFBW20LV.WA', 'ETFBW20ST.WA', 'ETFBW20TR.WA', 'ETFBWTECH.WA', 'ETFDAX.WA', 'ETFSP500.WA']
+companies = ['ALE.WA', 'PLW.WA', 'ANR.WA', 'CMP.WA', 'KGH.WA', 'MEX.WA']
 
 with st.sidebar:
      option = st.selectbox('Please select company', companies)
@@ -71,13 +71,13 @@ def plot():
           
     st.plotly_chart(fig)
      
-def details(): #Most on the variables below are no longer supported by yfinance (April 2023)
+def details():
     st.header("📝 Company's details")
 
     details = [
         ("Full name: ", df.info['longName']),
-        ("Sector: ", df.info['sector']), 
-        ("Industry: ", df.info['industry']), 
+        ("Sector: ", df.info['sector']),
+        ("Industry: ", df.info['industry']),
         ("Country: ", df.info['country']),
         ("City: ", df.info['city']),
         ("Address: ", df.info['address1']),
@@ -92,9 +92,6 @@ def details(): #Most on the variables below are no longer supported by yfinance 
 if option:
     df = yf.Ticker(option)
 
-    name = df.info['longName']
-    st.header(name)
-
     hist = df.history(period="max")
 
     tab1, tab2 = st.tabs(["Plot", "Raw Data"])
@@ -105,14 +102,41 @@ if option:
     with tab2:
         st.dataframe(hist)
 
+    with st.expander("📝 Company's details"):
+        details()
+          
+    with st.expander("💰 Financial Data"):
+     tab1, tab2, tab3 = st.tabs(["Financials", "Balance Sheet", "Cashflow"])
+
+    with tab1:
+        st.header("💵 Financial Statement")
+        st.dataframe(df.financials)
+
+    with tab2:
+        st.header("⚖️ Balance Sheet")
+        st.dataframe(df.balance_sheet)
+
+    with tab3:
+        st.header("💸 Cashflow")
+        st.dataframe(df.cashflow)
+
     with st.expander("🎯 Key Performance Indicators"):
      st.header('🎯 Key Performance Indicators')
      kpis = [
-        ('🔴 Market capitalization:  ',df.info['marketCap']),
-        ('🔴 Dividend yield:  ',df.info['trailingAnnualDividendYield']),
-        ('🔴 Price-to-earnings ratio (P/E):  ',df.info['trailingPE']),
-        ('🔴 Average analyst rating:  ',df.info['averageAnalystRating']),
-        ('🔴 Fifty-two week high/low:  ',df.info['fiftyTwoWeekRange'])
+        ('🔴 ebitdaMargins:  ',df.info['ebitdaMargins']),
+        ('🔴 profitMargins:  ',df.info['profitMargins']),
+        ('🔴 operatingCashflow:  ',df.info['operatingCashflow']),
+        ('🔴 revenueGrowth:  ',df.info['revenueGrowth']),
+        ('🔴 operatingMargins:  ',df.info['operatingMargins']),
+        ('🔴 earningsGrowth:  ',df.info['earningsGrowth']),
+        ('🔴 currentRatio:  ',df.info['currentRatio']),
+        ('🔴 returnOnAssets:  ',df.info['returnOnAssets']),
+        ('🔴 debtToEquity:  ',df.info['debtToEquity']),
+        ('🔴 returnOnEquity:  ',df.info['returnOnEquity']),
+        ('🔴 revenuePerShare:  ',df.info['revenuePerShare']),
+        ('🔴 quickRatio:  ',df.info['quickRatio']),
+        ('🔴 enterpriseToRevenue:  ',df.info['enterpriseToRevenue']),
+        ('🔴 enterpriseToEbitda:  ',df.info['enterpriseToEbitda'])
     ]
      for name, value in kpis:
           st.write(name, value)
@@ -170,46 +194,3 @@ if option:
          ])
      ))
      st.plotly_chart(fig)
-      
-    with st.expander("📊 Buy/Sell Signals"):
-     st.header('📊 Buy/Sell Signals')
-
-     df = hist
-     
-     df["SMA50"] = df["Close"].rolling(window=50).mean()
-     df["SMA200"] = df["Close"].rolling(window=200).mean()
-
-     df["Signal"] = 0
-     df.loc[df["SMA50"] > df["SMA200"], "Signal"] = 1
-     df["Position"] = df["Signal"].diff()
-     buy_signals = df[df["Position"] == 1]
-     sell_signals = df[df["Position"] == -1]
-     
-     current_signal = df["Signal"].tail(1).values[0]
-     if current_signal == 1:
-         st.header("Current signal: Buy")
-     else:
-         st.header("Current signal: Sell")
-
-     # Create a figure with one subplot
-     fig = go.Figure()
-
-     # Plot the stock price and moving averages
-     fig.add_trace(go.Scatter(x=df.index, y=df["Close"], name="Price"))
-     fig.add_trace(go.Scatter(x=df.index, y=df["SMA50"], name="50-day SMA"))
-     fig.add_trace(go.Scatter(x=df.index, y=df["SMA200"], name="200-day SMA"))
-
-     # Plot the buy and sell signals
-     fig.add_trace(go.Scatter(x=buy_signals.index, y=df.loc[buy_signals.index, "Close"], mode="markers", marker=dict(symbol="triangle-up", size=10, color="green"), name="Buy"))
-     fig.add_trace(go.Scatter(x=sell_signals.index, y=df.loc[sell_signals.index, "Close"], mode="markers", marker=dict(symbol="triangle-down", size=10, color="red"), name="Sell"))
-
-     fig.update_xaxes(rangeslider_visible=True, rangeselector=dict(
-         buttons=list([
-             dict(count=1, label="1m", step="month", stepmode="backward"),
-             dict(count=6, label="6m", step="month", stepmode="backward"),
-             dict(count=1, label="YTD", step="year", stepmode="todate"),
-             dict(count=1, label="1y", step="year", stepmode="backward"),
-             dict(step="all")
-         ])
-     ))
-     st.plotly_chart(fig)    
